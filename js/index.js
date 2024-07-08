b5123d60522a7d6cab815d0add64f6d6b30c2b79
PATOLINODD <psodesenhos@gmail.com>
2024-07-08 02:19:45 -0300

    Updates -> Mudancas na home page, novos estilos css

---
+++ js/index.js
@@ -0,0 +1,9 @@
+class Personagem {
+    constructor(nome, idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
+
+const artur = new Personagem("Artur Valeriano", 23);
+console.log(artur);
