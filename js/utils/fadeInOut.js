b0c0f001c1f229c6c1d2a20d3a7f7a6c24d98131
PATOLINODD <psodesenhos@gmail.com>
2024-07-16 18:07:40 -0300

    update -> animation on modal, validations at users typing

--- js/utils/fadeInOut.js
+++ js/utils/fadeInOut.js
@@ -3,8 +3,10 @@ document.addEventListener("DOMContentLoaded", ()=>{
         event.preventDefault();
         const modal = document.getElementById("form-data");
         modal.classList.toggle("show");
+        document.querySelector("body").classList.toggle("stopScrolling");
     }
 
     document.getElementById("consultoria").addEventListener("click", toggleModal);
+    document.getElementById("want_solution").addEventListener("click", toggleModal);
     document.getElementById("cancel").addEventListener("click", toggleModal);
 });
