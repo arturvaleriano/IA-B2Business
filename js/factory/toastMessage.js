b0fdec9ba326a9d2f32ce01d3523c72e4b8a9bb6
PATOLINODD <psodesenhos@gmail.com>
2024-07-18 20:30:01 -0300

    Update -> trying to show toast message flex column

--- js/factory/toastMessage.js
+++ js/factory/toastMessage.js
@@ -1,22 +1,33 @@
-function sendToastMessage(message, elementID, error) {
-  const element = document.querySelector(elementID);
-  const divContainer = document.createElement("div");
-  const messageError = document.createTextNode(message);
-  divContainer.appendChild(messageError);
-  divContainer.style = `
-  ${modelsColors[error]}
-    margin: 0 var(--margin-default) 1rem;
-    padding: 8px; 
+const divContainerFlex = document.createElement("div");
+  divContainerFlex.style = `
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    flex-flow: column wrap;
     position: absolute;
     right: 0;
     bottom: 0;
+  `;
+
+function sendToastMessage(message, elementID, error) {
+  const element = document.querySelector(elementID);
+  
+  const divMessage = document.createElement("div");
+  
+  const messageError = document.createTextNode(message);
+  divMessage.appendChild(messageError);
+  divMessage.style = `
+  ${modelsColors[error]}
+    margin: 0 var(--margin-default) 1rem;
+    padding: 8px;
     animation: toastMessageTemp 4s 1;
     transform: translate(100rem, 0);
     border: solid 1px var(--bs-green);
     border-radius: 1rem;
     `;
-
-  element.appendChild(divContainer);
+  divContainerFlex.appendChild(divMessage);
+  setTimeout(()=> {
+    divContainerFlex.removeChild(divMessage);
+  }, 4500);
+  element.appendChild(divContainerFlex);
 }
-
-sendToastMessage("Message Error!", "#form-data", Constants.error);
